de4363ed6d1e1cb039d951645be78e87502f902d
junsu502 <94041577+junsu502@users.noreply.github.com>
2021-11-12 12:16:10 +0900

    Add files via upload
    셀프넘버

---
+++ Proj_MMS2/src/mmms/member/action/Selfnumber.java
@@ -0,0 +1,19 @@
+package function;
+
+public class Selfnumber {
+
+	public static void main(String[] args) {
+
+//		d(33) = 33+3+3 = 39
+		int n = 0;
+//		= n+
+		String a = Integer.toString(n);
+	    for(int i=0; i<n; i++) {
+	    	a.charAt(i);
+	    }
+		
+		
+		
+	}
+
+}
